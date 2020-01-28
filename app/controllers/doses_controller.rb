5c587bbad98ff78dcfedea42c62d196a43c38c4c
Gurpriya Bhatia <gurpriya.bhatia@gmail.com>
2020-01-28 19:32:07 +0000

    Updated roots

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -17,13 +17,11 @@ class DosesController < ApplicationController
   end
 
   def destroy
-
     @dose = Dose.find(params[:id])
     @cocktail = @dose.cocktail
     @dose.destroy
 
     redirect_to cocktail_path(@cocktail)
-
   end
 
   private
